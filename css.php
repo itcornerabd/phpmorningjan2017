b165dea6c8c2400ea6c334bc7fd37737ba4268a5
dhiraj <ddw147@gmail.com>
2017-01-28 10:15:11 +0530

    css box model

--- css.php
+++ css.php
@@ -4,36 +4,23 @@
 	<title>Css Demo</title>
 
 	<style type="text/css">
+		/*inpage infile inline internal*/	
 		
-		span /* element wise selector- select all span element */
-		{
-			background-color: yellow;
-		}
-
-		.cls /* class wise selector select element havong cls as class */
-		{
-			font-size: 24px;	
-		}
-
-		#div1 /*id wise selector select element having div1 as id */
-		{
-			color: blue;	
-		}
-
 	</style>
 
+	<link rel="stylesheet" type="text/css" href="./style.css">
+	<!-- external css -->
 </head>
 <body>
 
+<div> Lorem ipsum dolor sit amet, ancillae oportere recteque eu vim, ex iracundia complectitur qui. Duo ea eius  </div>
+<div> div 2</div>
+<div> div 3</div>
 
-<span> span 1</span>
-<span>span 2 </span>
 
-
-<div id="div1"> div 1 </div>
-<div class="cls">div 2</div>
-
-<p class="cls"> p1 </p>
-<p>p2</p>
 </body>
-</html> 
+</html>
+
+
+
+
