b6bf82948f2c58f21530060e441a75e142a560c6
Itcorner <info@itcorner.org.in>
2017-02-01 10:24:16 +0530

    form processing

---
+++ action.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		
+	</title>
+</head>
+<body>
+
+	<?php 
+	 	//var_dump( $_GET);
+	 	
+	 	if(isset($_GET['id']))
+	 		echo "id is " . $_GET['id'];
+
+	 	if(isset($_GET['name']))
+	 		echo "<br>name is ".$_GET['name'];
+
+	 	//var_dump($_POST);
+
+	 	if(isset($_POST['name']))
+	 		echo "<br > name is ".$_POST['name'];
+
+	 	if(isset($_POST['password']))
+	 		echo "<br > password is ".$_POST['password'];
+
+	 	if(isset($_POST['address']))
+	 		echo "<br > address is ".$_POST['address'];
+
+	 	if(isset($_POST['city']))
+	 		{
+	 			echo "<br > city is ".$_POST['city'];
+	 			$city = $_POST['city'];
+
+	 		}
+
+	 	if(isset($_POST['gender']))
+	 		echo "<br > gender is ".$_POST['gender'];
+	 		
+	 	if(isset($_POST['SPORT']))
+	 		echo "<br > selected hobbby is ".$_POST['SPORT'];
+		if(isset($_POST['DANCE']))
+	 		echo "<br > selected hobbby is ".$_POST['DANCE'];
+		if(isset($_POST['SINGING']))
+	 		echo "<br > selected hobbby is ".$_POST['SINGING'];
+	
+		if(isset($_POST['hobby']))
+		{
+			$hobbies = $_POST['hobby'];
+			//var_dump($hobby);
+			echo "select hobbies are "; 
+			foreach ($hobbies as  $hobby) 
+			{
+				echo "$hobby ";
+			}
+
+
+		}	
+
+	 ?>
+
+</body>
+</html>
+
+
+
--- form.php
+++ form.php
@@ -15,34 +15,37 @@
 </head>
 <body>
 
-<form name="form1" id="form1" action="action.php" method="GET"  >
+
+<a href="action.php?id=1&name=abc">Link to action page</a>
+
+<form name="form1" id="form1" action="action.php" method="POST"  >
 	<center>
 	<table width="400px">
 		<tr>
 			<td> Name</td>
 			<td>
-				<input  type="text" name="name" id="name" placeholder="Enter your name here" required value="Dhiraj" >
+				<input  type="text" name="name" id="name" placeholder="Enter your name here"  >
 			</td>
 		</tr>
 		<tr>
 			<td>Password</td>
 			<td>
-				<input type="password" name="password" placeholder="Enter your password here" required value="123" >
+				<input type="password" name="password" placeholder="Enter your password here"  >
 			</td>
 		</tr>
 		<tr>
 			<td>Address</td>
 			<td>
-				<textarea name="address" required >asdasd</textarea>
+				<textarea name="address" ></textarea>
 			</td>
 		</tr>
 		<tr>
 			<td>City</td>
 			<td>
-				<select name="city" required>
+				<select name="city" >
 					<option value="">--select City--</option>
 					<option value="1" >A.bad</option>
-					<option value="2" selected >Pune</option>
+					<option value="2"  >Pune</option>
 					<option value="3"  >Mumbai</option>	
 				</select>
 			</td>
@@ -50,19 +53,30 @@
 		<tr>
 			<td>Gender</td>
 			<td>
-				<input type="radio" name="gender" value="MALE" checked required > MALE
-				<input type="radio" name="gender" value="FEMALE" required> FEMALE
+				<input type="radio" name="gender" value="MALE"  > MALE
+				<input type="radio" name="gender" value="FEMALE" > FEMALE
 			</td>
 		</tr>
 		<tr>
 			<td>Hobby</td>
 			<td>
-				<input type="checkbox" name="sport" value="SPORT" > SPORT
-				<input type="checkbox" name="DANCE" value="DANCE"  checked > DANCE
+				<input type="checkbox" name="SPORT" value="SPORT" > SPORT
+				<input type="checkbox" name="DANCE" value="DANCE"   > DANCE
 				<input type="checkbox" name="SINGING" value="SINGING"> SINGING
 
 			</td>
 		</tr>
+		<tr>
+			<td>Hobby</td>
+			<td>
+				<input type="checkbox" name="hobby[]" value="SPORT" > SPORT
+				<input type="checkbox" name="hobby[]" value="DANCE"   > DANCE
+				<input type="checkbox" name="hobby[]" value="SINGING"> SINGING
+				<input type="checkbox" name="hobby[]" value="PAINTING"> PAINTING
+
+			</td>
+		</tr>
+
 		<tr>
 			<td colspan="2">
 				<input type="button" name="" value="BUTTON">
